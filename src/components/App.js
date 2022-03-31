b345cd84ef4be86cb0d635c128632cdf85ab1db8
Monzer Mohammad <dev.monzer@gmail.com>
2022-03-31 16:34:22 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import RecipeList from "./RecipeList";
 import "../css/app.css";
+import uuidv4 from "uuid/v4";
 
 export default function App() {
+  const [recipes, setRecipes] = useState(sampleRecipes);
+
+  function handleRecipeAdd() {
+    const newRecipe = {
+      id: uuidv4(),
+      name: "New",
+      servings: 1,
+      cookTime: "1:00",
+      instructions: "Instr.",
+      ingredients: [{ id: uuidv4(), name: "Name", amount: "1 Tbs" }],
+    };
+
+    setRecipes([...recipes, newRecipe]);
+  }
+
   return (
     <>
-      <RecipeList recipes={sampleRecipes} />
+      <RecipeList recipes={recipes} />
     </>
   );
 }

01227ffcebd572ee9ff9035a8b1dbe78403f4484
Monzer Mohammad <dev.monzer@gmail.com>
2022-03-31 17:57:43 +0300

    Update RecipeList.jsx

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,20 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import Recipe from "./Recipe";
 
-export default function RecipeList(props) {
-  const { recipes, handleRecipeAdd, handleRecipeDelete } = props;
+import { RecipeContext } from "./App";
+
+export default function RecipeList({ recipes }) {
+  const { handleRecipeAdd } = useContext(RecipeContext);
 
   return (
     <div className="recipe-list">
       <div>
         {recipes.map((recipe) => {
-          return (
-            <Recipe
-              key={recipe.id}
-              recipe={recipe}
-              handleRecipeDelete={handleRecipeDelete}
-            />
-          );
+          return <Recipe key={recipe.id} recipe={recipe} />;
         })}
       </div>
       <div className="recipe-list__add-recipe-btn-container">

49293c1022649b30686e557d1df8f6d6873c5ed1
Monzer Mohammad <dev.monzer@gmail.com>
2022-03-31 21:01:34 +0300

    Update App.js
    added local storage settings

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import RecipeList from "./RecipeList";
 import "../css/app.css";
 import uuidv4 from "uuid/v4";
 
 // Creating a context to avoid props drillings
 export const RecipeContext = React.createContext();
+const LOCAL_STORAGE_KEY = "cookingWithReact.recipes";
 
 export default function App() {
   const [recipes, setRecipes] = useState(sampleRecipes);
+
+  useEffect(() => {
+    const recipeJSON = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (recipeJSON != null) setRecipes(JSON.parse(recipeJSON));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(recipes));
+  }, [recipes]);
 
   const RecipeValues = {
     handleRecipeAdd: handleRecipeAdd,

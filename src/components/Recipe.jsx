ecca432fc5c3c4b82b0379c13780aad0aaaa0bd2
Monzer Mohammad <dev.monzer@gmail.com>
2022-04-02 16:57:44 +0300

    Update Recipe.jsx

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -6,14 +6,19 @@ import { RecipeContext } from "./App";
 export default function Recipe({ recipe }) {
   const { id, name, cookTime, servings, instructions, ingredients } = recipe;
 
-  const { handleRecipeDelete } = useContext(RecipeContext);
+  const { handleRecipeDelete, handleRecipeSelect } = useContext(RecipeContext);
 
   return (
     <div className="recipe">
       <div className="recipe__header">
         <h3 className="recipe__title">{name}</h3>
         <div>
-          <button className="btn btn--primary mr-1">Edit</button>
+          <button
+            className="btn btn--primary mr-1"
+            onClick={() => handleRecipeSelect(id)}
+          >
+            Edit
+          </button>
           <button
             className="btn btn--danger"
             onClick={() => handleRecipeDelete(id)}

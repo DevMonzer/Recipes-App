d9bee33695fee517c0950b2e15fcc8b9104fd5c7
Monzer Mohammad <dev.monzer@gmail.com>
2022-04-03 23:39:15 +0300

    Update RecipeEdit.jsx

--- src/components/RecipeEdit.jsx
+++ src/components/RecipeEdit.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import RecipeIngredientEdit from "./RecipeIngredientEdit";
 import { RecipeContext } from "./App";
+import uuidv4 from "uuid/v4";
 
 export default function RecipeEdit({ recipe }) {
   const { handleRecipeChange, handleRecipeSelect } = useContext(RecipeContext);
@@ -16,6 +17,22 @@ export default function RecipeEdit({ recipe }) {
     const index = newIngredients.findIndex((i) => i.id === id);
     newIngredients[index] = ingredient;
     handleChange({ ingredients: newIngredients });
+  }
+
+  // Adding a new ingredient
+  function handleIngredientAdd() {
+    const newIngredient = {
+      id: uuidv4(),
+      name: "",
+      amount: "",
+    };
+    handleChange({ ingredients: [...recipe.ingredients, newIngredient] });
+  }
+
+  function handleIngredientDelete(id) {
+    handleChange({
+      ingredients: recipe.ingredients.filter((i) => i.id !== id),
+    });
   }
 
   return (

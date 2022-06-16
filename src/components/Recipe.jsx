d9d9f4ea16a89140868fb611e76150616fec843b
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-16 19:43:55 +0300

    Update Recipe.jsx

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -17,7 +17,7 @@ export default function Recipe({ recipe }) {
             className="btn btn--primary mr-1"
             onClick={() => handleRecipeSelect(id)}
           >
-            <span> Edit</span>
+            Edit
           </button>
           <button
             className="btn btn--danger"
